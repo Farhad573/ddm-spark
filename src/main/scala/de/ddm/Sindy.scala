ee58b37e24fddbcf2324005f403e651352a0426e
Sinafadavi <sina.fadavi1990@yahoo.com>
2024-02-01 20:53:56 +0100

    first part added

--- src/main/scala/de/ddm/Sindy.scala
+++ src/main/scala/de/ddm/Sindy.scala
@@ -16,6 +16,28 @@ object Sindy {
 
   def discoverINDs(inputs: List[String], spark: SparkSession): Unit = {
 
-    // TODO
+    import spark.implicits._
+    // Read data from input files and convert them into Spark Datasets
+    val datasets = inputs.map(input => readData(input, spark))
+    //    println("Datasets:")
+    //    datasets.foreach(println)
+
+    // Flatten datasets into tuples of (value, columnName)
+    val flattenedData =
+      datasets.map(ds => { // Spark Dataset -> tuples of value and corresponding columnName
+        val columns = ds.columns
+        ds.flatMap(row => {
+          for (i <- columns.indices) yield {
+            (row.getString(i), columns(i))
+          }
+        }) // (value, colName)
+      })
+    //    println("Flattened Data:")
+    //    flattenedData.foreach(println)
+
+    // Merge all datasets into a single dataset
+    val mergedData = flattenedData.reduce((ds1, ds2) => ds1.union(ds2))
+    //    println("Merged Data:")
+    //    mergedData.show()
   }
 }
