abb7f057e73aaeb058eaa198b00a19dad4292b1e
Salekim <Salekim@students.uni-marburg.de>
2024-02-01 23:31:34 +0100

    Second Part hinzufügt

--- src/main/scala/de/ddm/Sindy.scala
+++ src/main/scala/de/ddm/Sindy.scala
@@ -39,5 +39,20 @@ object Sindy {
     val mergedData = flattenedData.reduce((ds1, ds2) => ds1.union(ds2))
     //    println("Merged Data:")
     //    mergedData.show()
+    // Group by value to obtain unique values with their corresponding column names
+    val groupedData = mergedData.groupByKey(_._1)
+    //    println("Grouped Data:")
+    //    groupedData.mapGroups((key, data) => (key, data.toList)).show()
+
+    // Convert column names into sets to get unique column names for each value
+    val columnSets = groupedData.mapGroups((_, it) => it.map(_._2).toSet)
+    //    println("Column Sets:")
+    //    columnSets.show()
+
+    // Generate all possible combinations of elements in each set as candidates
+    val candidates = columnSets.flatMap(set =>
+      set.map(currentAttribute => (currentAttribute, set.filter(attribute => !attribute.equals(currentAttribute)))))
+    //    println("Candidates:")
+    //    candidates.show()
   }
 }
